36bdfdf9e28f605e3dff2ee49eb0cb7cbb9bef08
Emily Fan <emilyfan8@gmail.com>
2019-02-04 20:23:07 -0800

    Allow Spark encoders to “reset”
    Added resetEncoder() method and used encoderOffset as means to reset encoders
    Modified getEncoderPosition to use offset value
    Added comments

--- src/main/java/org/usfirst/frc/team2473/framework/CHS_SparkMax.java
+++ src/main/java/org/usfirst/frc/team2473/framework/CHS_SparkMax.java
@@ -10,7 +10,11 @@ public class CHS_SparkMax {
 	private CANSparkMax spark;
 	private CANEncoder encoder;
 
+	/* At times, the encoders randomly return zero by mistake */
 	private double lastEncoderValue = 0;
+	
+	/* Used to calculate the encoder position after a reset */
+	private double encoderOffset = 0;
 
 	public CHS_SparkMax(int port, MotorType motorType) {
 		spark = new CANSparkMax(port, motorType);
@@ -32,17 +36,33 @@ public class CHS_SparkMax {
 	public double getEncoderPosition() {
 		double position = encoder.getPosition();
 		if (position != 0) {
-			lastEncoderValue = position;
+			lastEncoderValue = position - encoderOffset;
 		}
 		
 		return lastEncoderValue;
 	}
 
-	public double getEncoderPositionUnmodified() {
-		return encoder.getPosition();
+	//Emily is confused as to the purpose of this method
+	// public double getEncoderPositionUnmodified() {
+	// 	return encoder.getPosition();
+	// }
+
+	/* Note that this may return 0 when it should NOT be zero */
+	public double getRawEncoderPosition() {
+	 	return encoder.getPosition();
 	}
 
 	public double getEncoderVelocity() {
 		return encoder.getVelocity();
+	}
+	/**
+	 * "Resets" the encoder by establishing an offset value that 
+	 * the getEncoderPosition() method would be based on
+	 * 
+	 * @return the offset value
+	 */
+	public double resetEncoder(){
+		encoderOffset = encoder.getPosition();
+		return encoderOffset;
 	}
 }
